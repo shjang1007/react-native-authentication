04259a871473080e75c38f0f894d7666c816ec02
Brian Jang <brian.jang91@gmail.com>
2022-02-20 17:02:02 -0500

    Clean code up removing what's not needed. Set up basic folder structure. Change npm run start and npm run ios

---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SignInScreen = () => {
+    return(
+        <View>
+            <Text>This is a "Sign In Screen"</Text>
+        </View>
+    )
+}
+
+export default SignInScreen;
