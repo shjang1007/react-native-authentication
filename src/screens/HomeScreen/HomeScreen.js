b3e4dab02153be02afd91771f7af02e93bbed03a
Brian Jang <brian.jang91@gmail.com>
2022-02-27 15:15:35 -0500

    Add some styling to home screen. Will need to take the run information out into modular component

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,64 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
 
 const Home = () => {
     return(
-        <View>
-            <Text>This is a Home Screen</Text>
+        <View style={ styles.container }>
+            <Text style={ styles.title }>Open Runs</Text>
+            <ScrollView>
+                <View style={ styles.runContainer }>
+                    <Text style={ styles.runTitle }>
+                        Really cool gym session
+                    </Text>
+                    <Text style={ styles.runDescription }>
+                        Hey guys, let's play nice and do our best to nurtuer a friendly environment.
+                    </Text>
+                    <View style={ styles.runDetails }>
+                        <FontAwesomeIcon icon={ faCircleUser } size={15}/>
+                        <Text style={ styles.runDetailText }>
+                            12/20 · $25 per person
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        padding: 20
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: "300",
+        marginBottom: 15,
+    },
+    runContainer: {
+        borderBottomWidth: 1,
+        borderColor: "black"
+    }, 
+    runTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginVertical: 8
+    },
+    runDescription: {
+        fontSize: 14,
+        marginVertical: 8
+    },
+    runDetails: {
+        flexDirection: "row",
+        marginVertical: 15
+    },
+    runDetailText: {
+        fontSize: 12,
+        fontWeight: "bold",
+        marginHorizontal: 3
+    }
+});
 
 export default Home;
