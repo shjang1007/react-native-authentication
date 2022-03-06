e324d69c7662b15290d7e5e26ebe382905f0bbe1
Brian Jang <brian.jang91@gmail.com>
2022-03-06 18:03:41 -0500

    Start implementing context api to handle userauthentication to refactor some code

---
+++ src/contexts/AuthContext/AuthContext.js
@@ -0,0 +1,8 @@
+import React, { createContext, useEffect, useState } from "react";
+import { Auth, Hub } from "aws-amplify";
+
+const AuthContext = createContext();
+
+export const AuthProvider = () {
+
+}
